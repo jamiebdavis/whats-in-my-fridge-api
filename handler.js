727954922c57f0a3a19f0ffab7a35c8c330f953d
Jamie Davis <jamie.davis@ao.com>
2021-07-26 13:43:10 +0100

    updated handler message

--- handler.js
+++ handler.js
@@ -1,15 +1,15 @@
 "use strict";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v2.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.hello = async event => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: "Go Serverless Whats in my fridge executed successfully!",
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
 };
